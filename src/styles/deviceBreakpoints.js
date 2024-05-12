19b04cbdfe187ec6c4bbac1248341a0ce0afde57
Davi Teixeira <daviteixeira.btm@gmail.com>
2024-05-12 02:59:33 -0300

    [Davi] Defining the breakpoints.

---
+++ src/styles/deviceBreakpoints.js
@@ -0,0 +1,9 @@
+export const DEVICE_BREAKPOINTS = {
+    MOBILE_S: "32.0rem",
+    MOBILE_M: "375px",
+    MOBILE_L: "42.5rem",
+    TABLET: "768px",
+    LAPTOP: "1024px",
+    LAPTOP_L: "1440px",
+    LAPTOP_4K: "2560px"
+}
