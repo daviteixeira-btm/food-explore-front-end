8ec461fa9c69a9602f2f856f6cdda8a303cbfe54
Davi Teixeira <daviteixeira.btm@gmail.com>
2024-05-15 12:30:59 -0300

    [Davi] Refactoring the button component.

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,15 +1,23 @@
 import styled from "styled-components";
 
 export const Container = styled.button`
+    gap: 1rem;
     border: 0;
     width: 100%;
+    display: flex;
     height: 4.8rem;
-    margin-top: 1.6rem;
     font-weight: 500;
+    margin-top: 1.6rem;
+    align-items: center;
     border-radius: 0.5rem;
     padding: 1.2rem 3.2rem;
+    justify-content: center;
     color: ${({ theme }) => theme.COLORS.LIGHT_100};
     background-color: ${({ theme }) => theme.COLORS.TOMATO_100};
+
+    > svg {
+        font-size: 2rem;
+    }
 
     &disabled {
         opacity: 0.5;
